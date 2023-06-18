aab7d6168d52dc0bcdcdbbd621ae6299d04632b9
MANHYIA <gmanhyia@gmail.com>
2023-06-18 09:47:19 -0700

    printing out the last digit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Description: "get the last digit"
+  *Return: always 0
+  */
+int main(void)
+{
+	int n, m;
+
+	for (n = 0; n <= 9; n++)
+	{
+	for (m = 0; m <= 8; m++)
+	}	
+}
