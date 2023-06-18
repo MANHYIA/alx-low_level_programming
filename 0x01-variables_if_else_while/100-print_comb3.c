dfc1bc621a0015db6acd10ad54651fd73764289c
MANHYIA <gmanhyia@gmail.com>
2023-06-18 10:34:03 -0700

    completed this exercise

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,17 +1,30 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
 /**
-  *main - Entry point
-  *Description: "get the last digit"
-  *Return: always 0
-  */
+ * main - prints all possible different combinations of two digits
+ * Return: ALways 0 (Success)
+ */
 int main(void)
 {
 	int n, m;
 
-	for (n = 0; n <= 9; n++)
+	for (n = 48; n <= 56; n++)
 	{
-	for (m = 0; m <= 8; m++)
-	}	
+		for (m = 49; m <= 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
+
